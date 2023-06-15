e63ad7b3efda9d7e35da7d55076edf1e7db01d89
Abdulkereem <abdulkereemokereem@gmail.com>
2023-06-15 15:19:48 +0100

    added soucre for execution

--- prezofinal-main/deploy.sh
+++ prezofinal-main/deploy.sh
@@ -1,8 +1,8 @@
 echo "Deploying... PrezzoShed"
 echo "staring django server..."
-./backed/api/start.sh
+source ./backed/api/start.sh
 echo "django server started"
 
 echo "Starting client side..."
 
-./prezzo/start.sh
+source ./prezzo/start.sh
