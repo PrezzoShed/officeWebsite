61805304b053c18d70d17788b62ce66aed69baf4
Abdulkereem <abdulkereemokereem@gmail.com>
2023-06-15 16:18:01 +0100

    final

--- prezofinal-main/prezzo/src/lib/config.ts
+++ prezofinal-main/prezzo/src/lib/config.ts
@@ -6,8 +6,8 @@ export let url : string
 export let mediaurl : string
 export let internalURL : string
 if(!production){
-     url = 'http://127.0.0.1:8000/'
-     mediaurl ='http://127.0.0.1:8000'
+     url = 'http://41.242.51.98:443/'
+     mediaurl ='http://41.242.51.98:443'
 
 }
 
